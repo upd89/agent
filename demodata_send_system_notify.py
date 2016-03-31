202d51ea6321be656be7377678be9842a5bbc254
Ueli Bosshard <ubosshar@hsr.ch>
2016-03-31 17:25:34 +0200

    lines moved

--- demodata_send_system_notify.py
+++ demodata_send_system_notify.py
@@ -4,10 +4,6 @@ import os,sys,time,json,urllib2
 from random import randint
 from json import JSONEncoder
 
-urn = 'vm1'
-#url = 'http://upd89.org/api.php'
-url = 'http://cc.upd89.org/v1/system/' + urn + '/notify'
-
 class System:
   def __init__(self, name, urn, os, address, reboot_required):
     self.name = name
@@ -20,7 +16,12 @@ class MyEncoder(JSONEncoder):
     def default(self, o):
         return o.__dict__
 
+# Demodata
+urn = 'vm1'
 sys = System(urn, urn, "Ubuntu 15.10", "127.0.0.1", True)
+
+#url = 'http://upd89.org/api.php'
+url = 'http://cc.upd89.org/v1/system/' + urn + '/notify'
 
 print("Sending to server (notify system " + urn + ")...")
 req = urllib2.Request(url)
