ff7b60486cac1371a4bdc01b1bc1a309c094e982
Ueli Bosshard <ubosshard@gmail.com>
2016-04-01 13:16:32 +0200

    send update data

--- demodata_send_system_notify.py
+++ demodata_send_system_notify.py
@@ -4,6 +4,14 @@ import os,sys,time,json,urllib2
 from random import randint
 from json import JSONEncoder
 
+class Update:
+  def __init__(self, name, version, arch, repository, baseversion):
+    self.name = name
+    self.version = version
+    self.architecture = arch
+    self.repository = repository
+    self.baseversion = baseversion
+
 class System:
   def __init__(self, name, urn, os, address, reboot_required):
     self.name = name
@@ -11,6 +19,10 @@ class System:
     self.os = os
     self.address = address
     self.reboot_required = reboot_required
+    self.packageupdates = list()
+
+  def addUpdate(self, update):
+    self.packageupdates.append(update)
 
 class MyEncoder(JSONEncoder):
     def default(self, o):
@@ -19,6 +31,8 @@ class MyEncoder(JSONEncoder):
 # Demodata
 urn = 'vm1'
 sys = System(urn, urn, "Ubuntu 15.10", "127.0.0.1", True)
+sys.addUpdate(Update(name="vim", version="demoupdate", arch="amd64", repository="demorepo", baseversion="2:7.4.712-2ubuntu4"))
+sys.addUpdate(Update(name="dnsutils", version="demoupdate", arch="amd64", repository="demorepo", baseversion="1:9.9.5.dfsg-11ubuntu1"))
 
 #url = 'http://upd89.org/api.php'
 url = 'http://cc.upd89.org/v1/system/' + urn + '/notify'
