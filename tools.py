38b41ab4b3495bb2e85303a5623c9de331275900
Ueli Bosshard <ubosshard@gmail.com>
2016-04-01 15:46:04 +0200

    productive register script added

---
+++ tools.py
@@ -0,0 +1,17 @@
+import socket
+
+def get_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        # doesn't even have to be reachable
+        s.connect(('10.255.255.255', 0))
+        IP = s.getsockname()[0]
+    except:
+        IP = '127.0.0.1'
+    finally:
+        s.close()
+    return IP
+
+# also possible:
+# my_ip = load(urlopen('http://jsonip.com'))['ip']
+
