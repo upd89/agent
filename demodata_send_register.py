13190f12158fb9cf2c0491540c837c38c03f434b
Ueli Bosshard <ubosshar@hsr.ch>
2016-04-01 14:07:01 +0200

    extracted classes

--- demodata_send_register.py
+++ demodata_send_register.py
@@ -2,22 +2,12 @@
 
 import os,sys,time,json,urllib2
 from random import randint
-from json import JSONEncoder
+
+from classes.system_register import System
+from classes.encoder import MyEncoder
 
 #url = 'http://upd89.org/api.php'
 url = 'http://cc.upd89.org/v1/register'
-
-class System:
-  def __init__(self, name, urn, os, address, tag):
-    self.name = name
-    self.urn = urn
-    self.os = os
-    self.address = address
-    self.tag = tag
-
-class MyEncoder(JSONEncoder):
-    def default(self, o):
-        return o.__dict__
 
 hostname = "vm" + str(randint(11,999))
 sys = System(hostname, "virt-" + hostname + "-nine", "Ubuntu 15.10", "127.0.0.1", "")
