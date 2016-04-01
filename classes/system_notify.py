e9a0954f864d000a9c33c9f39ca17f72063dff60
Ueli Bosshard <ubosshar@hsr.ch>
2016-04-01 14:03:21 +0200

    extracted classes

---
+++ classes/system_notify.py
@@ -0,0 +1,12 @@
+class System:
+  def __init__(self, name, urn, os, address, reboot_required):
+    self.name = name
+    self.urn = urn
+    self.os = os
+    self.address = address
+    self.reboot_required = reboot_required
+    self.packageupdates = list()
+
+  def addUpdate(self, update):
+    self.packageupdates.append(update)
+
