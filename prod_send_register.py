0b6919f4b0f76b419028260b5b0a2346ec652388
Ueli Bosshard <ubosshard@gmail.com>
2016-04-12 19:17:12 +0200

    cleanup again

--- prod_send_register.py
+++ prod_send_register.py
@@ -1,21 +1,14 @@
 #!/usr/bin/env python
 
 from lib.configloader.configloader import ConfigLoader
-from classes.system_register import System
 import lib.upstream
 import lib.sysinfo
 
 _config = ConfigLoader("config")
-url = lib.upstream.getRegisterURL(_config)
 
-# Data
-myHostname  = lib.sysinfo.get_hostname()
-myURN       = lib.sysinfo.get_urn()
-myDistro    = lib.sysinfo.get_distro()
-myIP        = lib.sysinfo.get_ip()
-sys = System(name=myHostname, urn=myURN, os=myDistro, address=myIP, tag="")
+sys = lib.sysinfo.get_register_system()
 
-print("Sending to server (register " + myHostname + ")...")
-response = lib.upstream.push(url, sys)
+print("Sending to server (register " + lib.sysinfo.get_hostname() + ")...")
+response = lib.upstream.pushRegister(_config, sys)
 print("Response:\n" + response)
 
