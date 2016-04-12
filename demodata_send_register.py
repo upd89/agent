5d6a26c0664ce996c9ef4a36665d1a6db221bb1f
Ueli Bosshard <ubosshar@hsr.ch>
2016-04-12 17:17:03 +0200

    big refactoring

--- demodata_send_register.py
+++ demodata_send_register.py
@@ -1,21 +1,19 @@
 #!/usr/bin/env python
 
-import os,sys,time,json,urllib2
 from random import randint
 
+from lib.configloader.configloader import ConfigLoader
 from classes.system_register import System
-from classes.encoder import MyEncoder
+import lib.upstream
 
-#url = 'http://upd89.org/api.php'
-url = 'http://cc.upd89.org/v1/register'
+_config = ConfigLoader("config")
+url = lib.upstream.getRegisterURL(_config)
 
+# Demodata
 hostname = "vm" + str(randint(11,999))
 sys = System(hostname, "virt-" + hostname + "-nine", "Ubuntu 15.10", "127.0.0.1", "")
 
 print("Sending to server (register " + hostname + ")...")
-req = urllib2.Request(url)
-req.add_header('Content-Type', 'application/json')
-response = urllib2.urlopen(req, json.dumps(sys, cls=MyEncoder))
-print("Response:")
-print(response.read())
+response = lib.upstream.push(url, sys)
+print("Response:\n" + response)
 
