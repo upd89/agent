93b4d3fd22ab6d0c631ff8bc416ea22cf50c58d9
Ueli Bosshard <ubosshard@gmail.com>
2016-04-14 11:23:01 +0200

    daemon with scheduler added

---
+++ lib/log/log.py
@@ -0,0 +1,19 @@
+import logging
+
+class Log:
+
+    def __init__(self, _config):
+        self.logger = logging.getLogger(__name__)
+        self.logger.setLevel(logging.DEBUG)
+        self.logger.propagate = False
+        self.fh = logging.FileHandler(_config.getLogFile(), "w")
+        self.fh.setLevel(logging.DEBUG)
+        self.logger.addHandler(self.fh)
+        self.keep_fds = [self.fh.stream.fileno()]
+
+    def getLogger(self):
+        return self.logger
+
+    def getKeepfds(self):
+        return self.keep_fds
+
