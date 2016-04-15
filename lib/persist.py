22391a5e50c87c00f072e8e162e44ae30de43309
Ueli Bosshard <ubosshar@hsr.ch>
2016-04-15 18:22:11 +0200

    persistens lib added

---
+++ lib/persist.py
@@ -0,0 +1,26 @@
+import shelve
+
+
+class Persist:
+
+    def __init__(self, filename):
+        self.store = shelve.open(filename)
+
+    def has_key(self, key):
+        return(self.store.has_key(key))
+
+    def delete_key(self, key):
+        if (has_key(key)):
+            del self.store[key]
+
+    def get_key(self, key):
+        if (has_key(key)):
+            return(self.store[key])
+        else:
+            return('')
+
+    def set_key(self, key, value):
+        self.store[key] = value
+
+    def close(self):
+        self.store.close()
