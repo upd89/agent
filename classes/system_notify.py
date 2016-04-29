50b9dad53d42773566839943d47bffee30b7cb8d
Ueli Bosshard <ubosshard@gmail.com>
2016-04-29 18:10:13 +0200

    preparation for new api

--- classes/system_notify.py
+++ classes/system_notify.py
@@ -6,7 +6,9 @@ class System:
         self.os = os
         self.address = address
         self.reboot_required = reboot_required
+        self.updCount = 0
         self.packageupdates = list()
 
     def addUpdate(self, update):
         self.packageupdates.append(update)
+        self.updCount += 1
