9ff398fb40a30b61d0ece0d55ff35d14e77e1b13
Philipp Christen <philipp.christen@gmail.com>
2016-04-29 18:10:13 +0200

    preparation for new api

--- classes/packagelist.py
+++ classes/packagelist.py
@@ -2,6 +2,8 @@ class Packagelist:
 
     def __init__(self):
         self.packages = list()
+        self.pkgCount = 0
 
     def add(self, package):
         self.packages.append(package)
+        self.pkgCount =+ 1
---
+++ classes/repository.py
@@ -0,0 +1,6 @@
+class Repository:
+
+    def __init__(self, archive, origin, component):
+        self.archive = archive
+        self.origin = origin
+        self.component = component
