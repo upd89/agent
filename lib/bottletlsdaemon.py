64068cc9fd0e5b2ace304a91eefa32b16965b9a8
Ueli Bosshard <ubosshard@gmail.com>
2016-05-09 10:17:56 +0200

    moved pem file into certs/

--- lib/bottletlsdaemon.py
+++ lib/bottletlsdaemon.py
@@ -20,7 +20,7 @@ class SSLWSGIRefServer(bottle.ServerAdapter):
         srv = make_server(self.host, self.port, handler, **self.options)
         srv.socket = ssl.wrap_socket (
          srv.socket,
-         certfile='/opt/upd89/agent/server.pem',  # path to certificate
+         certfile='/opt/upd89/agent/certs/server.pem',  # path to certificate
          server_side=True)
         srv.serve_forever()
 
