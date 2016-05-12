e6e7af485ea006c9c79f67b75bbd543a73421998
Ueli Bosshard <ubosshard@gmail.com>
2016-05-12 11:16:38 +0200

    autopep8

--- tools/syspath.py
+++ tools/syspath.py
@@ -1,8 +1,9 @@
-import os,sys,inspect
+import os
+import sys
+import inspect
 
 cmd_folder = os.path.realpath(os.path.abspath(os.path.split(inspect.getfile(
-        inspect.currentframe() ))[0])+ "/..")
+                                                            inspect.currentframe()))[0]) + "/..")
 
 if cmd_folder not in sys.path:
         sys.path.insert(0, cmd_folder)
-
