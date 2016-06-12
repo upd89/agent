687f896ecaf70c7d534dc926917616ecc756a818
Ueli Bosshard <ubosshard@gmail.com>
2016-06-12 18:19:33 +0200

    added Manifest-File

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name = "upd89",
-    version = "0.5.19",
+    version = "0.5.21",
     author = "Ueli Bosshard",
     author_email = "ubosshard@gmail.com",
     description = ("upd89 is a system update management "
